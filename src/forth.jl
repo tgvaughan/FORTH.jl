e7ffe0f277ef1d2b9b9005e01e2149b4d34190f7
Tim Vaughan <tgvaughan@gmail.com>
2016-04-04 13:16:40 +1200

    Bootstrapping outer interpreter.

--- src/forth.jl
+++ src/forth.jl
@@ -1,23 +1,52 @@
-module FORTH
+module forth
 
-function input(prompt::AbstractString="")
-    print(prompt)
-    strip(readline)
+currentLine = ""
+currentPos = 0
+
+function nextLine()
+    if eof(STDIN)
+        return false
+    else
+        currentLine = readLine()
+        currentPos = 1
+        return true
+    end
 end
 
-dictCore = Set{AbstractString}([
-    "bye"
-])
+function readPattern(pattern::Regex)
+    m = match(pattern, currentLine)
+    pos += length(m.match)
+    return m.match
+end
 
-function coreEval(word::AbstractString)
-    if word == "bye"
+modes = Dict{AbstractString,Function}()
+mode = ""
+
+function interpretPrimitive()
+    if haskey(dict, word)
+        dict[word]()
+        return true
+    else
+        return false
+end
+interpretNonPrimitive() = false
+interpretNumber() = false
+
+modes["interpret"] = () -> begin
+    getWord()
+    
+    if ! (interpretPrimitive() ||
+        interpretNonPrimitive() ||
+        interpretNumber())
+        println("Error: unknown word '$word'.")
+    end
 end
 
 function repl()
 
-    while true
-        line = input()
-
+    mode = "interpret"
+    while mode != "stop"
+        modes[mode]()
     end
 end
 
